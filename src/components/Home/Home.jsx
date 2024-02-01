d667c5d14ef598cedf2f78b484745b227e3bede5
Div-AHM1ED <ahmedabdelaleem100@gmail.com>
2024-02-01 17:28:15 +0200

    yfarag

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   const location = useLocation();
   const {  pathname } = location;
   let currentPath = pathname.slice(1);
-  let [link, setLink] = useState('#');
+  let [link, setLink] = useState();
   useEffect(() => {
     setLink(currentPath || 'START FRAMEWORK')
   }, [location.pathname]);
